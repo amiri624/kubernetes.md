a0f1fe7b9e57f38685602ea0faffbb9c0a505e9d
Meisam <meisamamiri2020@gmail.com>
2025-02-04 19:23:07 +0100

    Delete minikube.sh

--- minikube.sh
+++
@@ -1,6 +0,0 @@
-# Download and run the installer for the latest release.
-# Or if using PowerShell, use this command:
-```bash
-New-Item -Path 'c:\' -Name 'minikube' -ItemType Directory -Force
-Invoke-WebRequest -OutFile 'c:\minikube\minikube.exe' -Uri 'https://github.com/kubernetes/minikube/releases/latest/download/minikube-windows-amd64.exe' -UseBasicParsing
-```
